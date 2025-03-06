6579780207d02ed17f48923efb9af88587a2bfe8
Иван <dreamscometrueivan97@gmail.com>
2025-03-06 17:30:39 +0200

    first-push

---
+++ main/java/com/example/projectforwork/ProjectForWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectforwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectForWorkApplication.class, args);
+    }
+
+}
